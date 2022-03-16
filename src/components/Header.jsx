04ffd536bf829fb55c0ff6b7f07fa83801ea0846
maraquepo <maraquepo@gmail.com>
2022-03-16 15:37:44 -0500

    init commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Header.css";
+import SearchIcon from '@mui/icons-material/Search';
+
+const Header = () => {
+  return (
+    <div>
+      <div className='Title'>NBA TEAMS</div>
+      <div className='searchInput'>
+        <input type="text" placeholder="Search team..." />
+        <div className="searchIcon">
+          <SearchIcon/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
