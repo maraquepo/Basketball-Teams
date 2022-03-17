006a4b8168bea38890751b1b068e9d3d4655d827
maraquepo <maraquepo@gmail.com>
2022-03-17 01:42:22 -0500

    added sample list items, will use map function with sample api tmrw

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,12 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Table.css'
-import Button from 'react-bootstrap/Button';
-
+import { Card, CardGroup } from 'react-bootstrap';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 const Table = () => {
+  const [isUp, setIsUp] = useState(false)
+
+  const func = () => {
+    setIsUp(prev => !prev)
+  }
+
   return (
     <div className='tableContainer'>
-
+      <CardGroup className='tableTitles'>
+        <Card body>Team Name</Card>
+        <Card body>City
+          {isUp
+          ? <ArrowDropUpIcon onClick={func} style={{cursor: 'pointer'}}/>
+          : <ArrowDropDownIcon onClick={func} style={{cursor: 'pointer'}}/> }
+        </Card>
+        <Card body>Abbreviation</Card>
+        <Card body>Conference</Card>
+        <Card body>Division</Card>
+      </CardGroup>
+      <CardGroup className='tableData'>
+        <Card body>Hawks</Card>
+        <Card body>Altanta</Card>
+        <Card body>ALT</Card>
+        <Card body>East</Card>
+        <Card body>Southeast</Card>
+      </CardGroup>
+      <CardGroup className='tableData'>
+        <Card body>Hawks</Card>
+        <Card body>Altanta</Card>
+        <Card body>ALT</Card>
+        <Card body>East</Card>
+        <Card body>Southeast</Card>
+      </CardGroup>
 
     </div>
   )
